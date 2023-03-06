fe921d48f102eb22d91bce0252c95ef14a4321fb
Shohei Kikuchi <killchance2256@gmail.com>
2023-03-06 20:17:43 +0900

    feat: ツイート内容をCLIの引数を渡すように変更

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const puppeteer = require('puppeteer');
-require('dotenv').config();
+const { argv } = require("process");
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+const tweet = process.argv;
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,  // 動作確認するためheadlessモードにしない
-    slowMo: 500  // 動作確認しやすいようにpuppeteerの操作を遅延させる
-  })
-  const page = await browser.newPage()
+    headless: false, // 動作確認するためheadlessモードにしない
+    slowMo: 500, // 動作確認しやすいようにpuppeteerの操作を遅延させる
+  });
+  const page = await browser.newPage();
 
-  await page.setViewport({ width: 720, height: 600 })
 
-  await page.goto('https://twitter.com/')  // Twitterのホームページに移動
-  await page.click('a[href="/login"]') //ログインボタンをクリック
-  await page.type('input[name="text"]', 'skikuchi7329') //ログインIDを入力
-  await page.keyboard.press('Enter') //エンターキーを押して次へ（ボタンを選択もOK)
-  await page.type('input[name="password"]', `${process.env.password}`) //パスワードを入力
-  await page.keyboard.press('Enter')
-  new Promise(r => setTimeout(r,5000)) //５秒まつ
-  await page.click('a[href^="/compose/tweet"]') //新規ツイートボタンをクリック
-  await page.type('.DraftEditor-root', 'これはpuppeteerからのツイートです') //ツイート内容を入力
-  await page.screenshot({ path: 'screenshot/sample1.png' }, { delay: 5000}) //ツイート内容を入力した状態でスクリーンショットを撮る
-  await page.click('div[data-testid="tweetButton"]') //ツイートする
-  
+  await page.setViewport({ width: 720, height: 600 });
+
+  await page.goto("https://twitter.com/"); // Twitterのホームページに移動
+  await page.click('a[href="/login"]'); //ログインボタンをクリック
+  await page.type('input[name="text"]', "skikuchi7329"); //ログインIDを入力
+  await page.keyboard.press("Enter"); //エンターキーを押して次へ（ボタンを選択もOK)
+  await page.type('input[name="password"]', `${process.env.password}`); //パスワードを入力
+  await page.keyboard.press("Enter");
+  await new Promise((r) => setTimeout(r, 5000)); //５秒まつ
+  await page.click('a[href^="/compose/tweet"]'); //新規ツイートボタンをクリック
+  await page.type(".DraftEditor-root", `${process.argv[2]}`); //ツイート内容をCLIの引数として入力
+  await page.screenshot({ path: "screenshot/sample1.png" }, { delay: 5000 }); //ツイート内容を入力した状態でスクリーンショットを撮る
+  await page.click('div[data-testid="tweetButton"]'); //ツイートする
+
   await browser.close();
-})()
+})();
